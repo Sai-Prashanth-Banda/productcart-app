68f39e6e8e101308f53b2e58dbe1d5c554d64c67
Sai Prashanth <65449720+Sai-Prashanth-Banda@users.noreply.github.com>
2022-06-25 00:50:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Badge from './Badge';
+import Card from './Card';
+
+function App() {
+  return (
+    <div>
+    <Badge/>
+    <Card/>
+    </div>
+  );
+}
+
+export default App;
